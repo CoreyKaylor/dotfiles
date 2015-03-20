3c1ed9fdd291b22ee62220f6365dda99965e9848
Corey Kaylor <corey@kaylors.net>
2015-03-20 13:45:04 -0600

    Little bit better git setup

---
+++ _emacs.d/config/git-gutter+.el
@@ -0,0 +1,6 @@
+(use-package git-gutter+
+  :ensure git-gutter+
+  :config (global-git-gutter+-mode))
+
+(use-package git-gutter-fringe+
+  :ensure git-gutter-fringe+)
--- _emacs.d/config/magit.el
+++ _emacs.d/config/magit.el
@@ -6,12 +6,6 @@
     (evil-set-initial-state 'magit-status-mode 'normal)
     (evil-set-initial-state 'magit-diff-mode 'normal)
     (evil-set-initial-state 'magit-log-mode 'normal)
-    (evil-define-key 'normal magit-mode-map
-        "j" 'magit-goto-next-section
-        "k" 'magit-goto-previous-section)
-    (evil-define-key 'normal magit-log-mode-map
-        "j" 'magit-goto-next-section
-        "k" 'magit-goto-previous-section)
-    (evil-define-key 'normal magit-diff-mode-map
-        "j" 'magit-goto-next-section
-        "k" 'magit-goto-previous-section)))
+    (evil-define-key 'normal magit-mode-map "j" 'magit-goto-next-section "k" 'magit-goto-previous-section)
+    (evil-define-key 'normal magit-log-mode-map "j" 'magit-goto-next-section "k" 'magit-goto-previous-section)
+    (evil-define-key 'normal magit-diff-mode-map "j" 'magit-goto-next-section "k" 'magit-goto-previous-section)))
